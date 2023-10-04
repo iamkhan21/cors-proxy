24031cbbe438603d1df0991085a3619fc2fd6b82
Ivan Khanevich <mail@iamkhan.tech>
2023-10-04 22:57:31 +0400

    feat: Update CORS Proxy page messaging
    Changed the CORS Proxy service landing page to provide clearer instructions on how to use the service. The code was modified to return HTML content instead of just text, improving user experience and understanding of the functionality.

--- src/routes/cors-proxy.ts
+++ src/routes/cors-proxy.ts
@@ -5,7 +5,12 @@ export function handleCorsProxy(): Handler {
     const targetUrl = c.req.query("target");
 
     if (!targetUrl) {
-      return c.text(`Hello at "CORS Proxy" by 8byte.agency!`);
+      return c.html(`
+      <section style="font-family: Arial, sans-serif; padding: 20px; text-align: center;">
+        <h1>Welcome to the "CORS Proxy" service by 8byte.agency!</h1>
+        <p>To use this service, append your target URL as a query parameter like so: ?target=YOUR_URL_HERE</p>
+      </section>
+    `);
     }
 
     const response = await fetch(targetUrl, c.req);
