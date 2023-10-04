82ef10e372c01f3faba79fb022b68fa7a7ba6ad7
Ivan Khanevich <mail@iamkhan.tech>
2023-10-05 00:07:27 +0400

    refactor: update CORS proxy welcome message and prevent self-targeting
    Updated the welcome message for the CORS proxy service to showcase our agency. Also added a validation to prevent self-targeting in the CORS Proxy to avoid potential infinite loops or unintended redirection outcomes.

--- src/routes/cors-proxy.ts
+++ src/routes/cors-proxy.ts
@@ -6,16 +6,41 @@ export function handleCorsProxy(): Handler {
 
     if (!targetUrl) {
       return c.html(`
-      <section style="font-size: 1.2rem; font-family: Arial, sans-serif; padding: 20px; text-align: center;">
-        <h3>Welcome to the "CORS Proxy" service by 8byte.agency!</h3>
-        <p>
-          To use this service, append your target URL as a query parameter like so:<br/>
-          <b>
-             https://cors.iamkhan.tech?target=YOUR_URL_HERE
-          </b>
-        </p>       
-      </section>
+        <section
+          style="
+            font-size: 1.2rem;
+            font-family: Arial, sans-serif;
+            padding: 20px;
+            text-align: center;
+          "
+        >
+          <h3>Welcome to the "CORS Proxy" service!</h3>
+          <p>
+            To use this service, append your target URL as a query parameter like
+            so:<br />
+            <b> https://cors.iamkhan.tech/?target=YOUR_URL_HERE </b>
+          </p>
+          <footer>
+            <small>
+              Created by
+              <a
+                href="https://www.8byte.agency"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                8byte Agency
+              </a>
+            </small>
+          </footer>
+        </section>
     `);
+    } else {
+      const proxyHost = new URL(c.req.url).origin;
+      const targetHost = new URL(targetUrl).origin;
+
+      if (proxyHost === targetHost) {
+        return c.text("Error: Self-targeting is not allowed.", 400);
+      }
     }
 
     const response = await fetch(targetUrl, c.req);
