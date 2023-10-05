265355548b2973f23ca17523d91eb4bd8b454cc0
Ivan Khanevich <mail@iamkhan.tech>
2023-10-05 19:52:00 +0400

    feat: add security features to CORS proxy
    Added URL validation and restrictions on certain file extensions and content types in CORS proxy. This prevents fetching of potentially harmful files (like .php, .exe, .js) and content types (like "application/javascript" or "application/php") to enhance application security. Also added error handling for invalid URLs.

--- src/routes/cors-proxy.ts
+++ src/routes/cors-proxy.ts
@@ -6,16 +6,47 @@ export function handleCorsProxy(): Handler {
 
     if (!targetUrl) {
       return c.redirect(`/`);
-    } else {
-      const proxyHost = new URL(c.req.url).origin;
-      const targetHost = new URL(targetUrl).origin;
+    }
 
-      if (proxyHost === targetHost) {
-        return c.text("Error: Self-targeting is not allowed.", 400);
+    // Validate URL
+    let targetHost;
+    try {
+      targetHost = new URL(targetUrl).origin;
+    } catch (e) {
+      return c.text("Error: Invalid URL.", 400);
+    }
+
+    const proxyHost = new URL(c.req.url).origin;
+
+    if (proxyHost === targetHost) {
+      return c.text("Error: Self-targeting is not allowed.", 400);
+    }
+
+    // Check for file extensions (this is a basic check, you can extend this list)
+    const disallowedExtensions = [".php", ".exe", ".js"];
+    for (let ext of disallowedExtensions) {
+      if (targetUrl.endsWith(ext)) {
+        return c.text(
+          `Error: Fetching files with ${ext} extension is not allowed.`,
+          400,
+        );
       }
     }
 
     const response = await fetch(targetUrl, c.req);
+
+    // Check Content-Type (this is a basic check, you can extend this list)
+    const contentType = response.headers.get("Content-Type");
+    if (
+      contentType &&
+      (contentType.startsWith("application/javascript") ||
+        contentType.startsWith("application/php"))
+    ) {
+      return c.text(
+        "Error: Fetching this type of content is not allowed.",
+        400,
+      );
+    }
 
     return new Response(response.body, {
       status: response.status,
